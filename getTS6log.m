011a3d9a54dab97a42b8d78ec0c9c4ad3c65aa00
tanabe-lab <45352048+keisuki42195@users.noreply.github.com>
2022-01-21 19:37:00 +0900

    macyou

--- getTS6log.m
+++ getTS6log.m
@@ -1,9 +1,9 @@
 function [ts6log]=getTS6log(DOCID)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Google spread sheetをcsvにしてMatlabに変数として読み込む
-% [DOCID]はGoogle spread sheetのリンクのhttps://docs.google.com/spreadsheets/d/のあとの部分
+% Google spread sheetをcsvにしてMatlabに変数として読み込�?
+% [DOCID]はGoogle spread sheetのリンクのhttps://docs.google.com/spreadsheets/d/のあとの部�?
 % [DOCID] A value like '0AmQ013fj5234gSXFAWLK1REgwRW02hsd3c', which is found in your spreadsheet's url: https://docs.google.com/spreadsheets/d/<here>/edit#gid=0.
-% 参考：https://jp.mathworks.com/matlabcentral/fileexchange/39915-getgooglespreadsheet
+% 参�??��https://jp.mathworks.com/matlabcentral/fileexchange/39915-getgooglespreadsheet
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 loginURL = 'https://www.google.com';
@@ -14,5 +14,11 @@ handler = sun.net.www.protocol.https.Handler;
 connection = java.net.URL([],loginURL,handler).openConnection();
 connection.getInputStream();
 
-ts6log = webread(csvURL);
+options=weboptions('CharacterEncoding','UTF-8');
+
+filename='ts6log.csv';
+outfilename=websave(filename,csvURL,options);
+opts=detectImportOptions(filename);
+%ts6log = webread(csvURL,options);
+ts6log=readtable(outfilename,opts);
 end
