a1ab475dc1ac4f781868e2f3fc92c5b5725f08ba
kdoi-lab <103361981+kdoi-lab@users.noreply.github.com>
2022-05-12 15:43:55 +0900

    add main_pcb.m
    main_pcb.mを実行すれば、実験ログの検索、解析shotの指定、時系列磁気面プロットができる。
    main_pcb_dtacq.mは自分でdtacqのshot番号や実験パラメータを指定したい場合用に作成。

--- pcb/pcbdata.m
+++ pcb/pcbdata.m
@@ -12,8 +12,9 @@ if exist(coeff)==0
     load('rc_coeff2020.mat')
 end     
 [ok, bz, rpos, zpos,p_ch] = getpcbbz(rawdata, coeff,date);
-% チャンネルごとの信号
-%bz=smoothdata(bz,1);
+
+% チャンネルごとの生信号のプロット
+% bz_s=smoothdata(bz,1);
 % r = 5;
 % col1 = 12;
 % col2 = 13;
@@ -22,13 +23,12 @@ end
 % t_start=300;
 % t_end=600;
 % r_ch=col1+col2;
-% plotbzsignal(y_upper_lim, col2, col1, t_end, p_ch, y_lower_lim, t_start, bz, ok, r_ch, r);
+% plotbzsignal(y_upper_lim, col2, col1, t_end, p_ch, y_lower_lim, t_start, bz_s, ok, r_ch, r);
 % clear r col1 col2 y_upper_lim y_lower_lim t t_start t_end
 
 [zq,rq]=meshgrid(linspace(min(zpos),max(zpos),n),linspace(min(rpos),max(rpos),n));
 grid2D=struct('zq',zq,'rq',rq);
 clear zq rq
-
 
 data2D = data2Dcalc(EF, grid2D, n, trange, rpos, zpos, bz, ok);
 end
--- pcb/plotbzsignal.m
+++ pcb/plotbzsignal.m
@@ -3,13 +3,14 @@ figure('Position', [10 10 1200 900])
 for i=1:r
     for j=1:col1
         subplot(r,col1,(i-1)*col1+j)
-        if ok(r_ch*(i-1)+j)==1 
-            plot(bz(t_start:t_end,r_ch*(i-1)+j))
-        else 
-            plot(bz(t_start:t_end,r_ch*(i-1)+j),'r:')
+        if ok(r_ch*(i-1)+j)==1 %okなチャンネルはそのままプロット
+            plot(t_start:t_end,bz(t_start:t_end,r_ch*(i-1)+j))
+        else %NGなチャンネルは赤色点線でプロット
+            plot(t_start:t_end,bz(t_start:t_end,r_ch*(i-1)+j),'r:')
         end   
         title(num2str(p_ch(i,j)));
         %xlim([t_start t_end]);
+        xticks([t_start t_end]);
         ylim([y_lower_lim y_upper_lim]);
     end
 end
@@ -19,12 +20,13 @@ for i=1:r
     for j=col1+1:col1+col2
         subplot(r,col2,(i-1)*col2+j-col1)
         if ok(r_ch*(i-1)+j)==1 
-            plot(bz(t_start:t_end,r_ch*(i-1)+j))
+            plot(t_start:t_end,bz(t_start:t_end,r_ch*(i-1)+j))
         else 
-            plot(bz(t_start:t_end,r_ch*(i-1)+j),'r:')
+            plot(t_start:t_end,bz(t_start:t_end,r_ch*(i-1)+j),'r:')
         end   
         title(num2str(p_ch(i,j)));
         %xlim([t_start t_end]);
+        xticks([t_start t_end]);
         ylim([y_lower_lim y_upper_lim]);
     end
 end
