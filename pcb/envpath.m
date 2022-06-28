baf1bcf930b9409c88b58720de8df7cdb7c6cd8b
kdoi-lab <103361981+kdoi-lab@users.noreply.github.com>
2022-06-28 17:38:23 +0900

    Update envpath.m
    windowsからのtest

--- pcb/envpath.m
+++ pcb/envpath.m
@@ -1,4 +1,5 @@
 %%%環境変数の設定
+%%%macの場合
 % setenv('ts3u_path', '/Users/mgar/koala/mnt/koala-experiment/results/ts-3u');
 % setenv('fourier_path','/Users/mgar/koala/mnt/fourier');
 % setenv('NIFS_path','/Users/mgar/koala/mnt/koala-experiment/results');
