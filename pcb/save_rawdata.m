e49502e429fb0ebf7dc2f713123011d96cb57df9
kdoi-lab <103361981+kdoi-lab@users.noreply.github.com>
2022-10-19 16:19:58 +0900

    rawdataへの変換
    MDSplusの入ったPCでdtacqデータをrawdataに変換する（TFoffsetを引かない場合）

--- pcb/save_rawdata.m
+++ pcb/save_rawdata.m
@@ -1,25 +1,38 @@
 %1000*128のrawdataをmat形式で保存
 
-%%%%(1)spread sheetから ログのテーブルを取得してTに格納
-%Github/test-open/getTS6log.mを使用
-DOCID='1wG5fBaiQ7-jOzOI-2pkPAeV6SDiHc_LrOdcbWlvhHBw';%スプレッドシートのID
-T=getTS6log(DOCID);
+% %%%%(1)spread sheetから ログのテーブルを取得してTに格納
+% %Github/test-open/getTS6log.mを使用
+% DOCID='1wG5fBaiQ7-jOzOI-2pkPAeV6SDiHc_LrOdcbWlvhHBw';%スプレッドシートのID
+% T=getTS6log(DOCID);
+% 
+% pathname.rawdata='/Users/mgar/rawdata_a038_nooffset/'; %保存先
+% 
+% IDXlist=1;%1:2950;%2452:2950;
+% % Nan_id=isnan(T.d_tacq(IDXlist));
+% Nan1=isnan(T.d_tacq(IDXlist));
+% Nan2=isnan(T.TFdtacq(IDXlist));
+% Nan=Nan1|Nan2;
+% IDXlist(Nan)=[];
+% 
+% 
+% %%%%(3)指定したshotの解析
+% %IDXlist=[2911:2913 2925 2926 2927 2931 2933 2947:2950 2942 2943 2946];
+% for IDX=IDXlist(1,:)
+%     d_tacq=T.d_tacq(IDX);
+%     d_tacqTF=T.TFdtacq(IDX);
+%     [rawdata]=getvalue(d_tacq,d_tacqTF);
+%     save(strcat(pathname.rawdata,'rawdata_dtacq',num2str(d_tacq),'.mat'),'rawdata');
+% end
 
-pathname.rawdata='/Users/mgar/rawdata_a038/'; %保存先
+%%%offsetなしのrawdata
+pathname.rawdata='/Users/mgar/rawdata_a038_noTF/'; %保存先
+dtacqlist=10005:10007;%IDXではなくdtacqの通し番号
 
-IDXlist=2452:2950;
-% Nan_id=isnan(T.d_tacq(IDXlist));
-Nan1=isnan(T.d_tacq(IDXlist));
-Nan2=isnan(T.TFdtacq(IDXlist));
-Nan=Nan1|Nan2;
-IDXlist(Nan)=[];
-
-
-%%%%(3)指定したshotの解析
-%IDXlist=[2911:2913 2925 2926 2927 2931 2933 2947:2950 2942 2943 2946];
-for IDX=IDXlist(1,:)
-    d_tacq=T.d_tacq(IDX);
-    d_tacqTF=T.TFdtacq(IDX);
-    [rawdata]=getvalue(d_tacq,d_tacqTF);
-    save(strcat(pathname.rawdata,'rawdata_dtacq',num2str(d_tacq),'.mat'),'rawdata');
+for dtacq=dtacqlist(1,:)
+    clear x
+    [rawdata]=getvalue_noTF(dtacq);
+    save(strcat(pathname.rawdata,'rawdata_noTF_dtacq',num2str(dtacq),'.mat'),'rawdata');
 end
+
+
+
