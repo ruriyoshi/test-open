e676a961203c2ee712eaf0876ef48ca4d7dec702
shinjiro1214 <shinjiro.takeda@gmail.com>
2022-12-02 22:30:58 +0900

    最小Fisher情報量法の導入（途中経過）

--- Soft X-ray/plot_SXR_at_t.m
+++ Soft X-ray/plot_SXR_at_t.m
@@ -1,4 +1,4 @@
-function [EE_high,EE_low] = plot_SXR_at_t(B_z,r_probe,z_probe,date,shot,t,show_xpoint,show_localmax,start,interval,save,SXRfilename,filter)
+function [EE_high,EE_low] = plot_SXR_at_t(B_z,r_probe,z_probe,date,shot,t,show_xpoint,show_localmax,start,interval,save,SXRfilename,filter,NL)
 % plot SXR emission on psi in rz plane
 % input:
 %   3d array of double: B_z (r,z,t), offsetted at zero and smoothed
@@ -14,6 +14,7 @@ function [EE_high,EE_low] = plot_SXR_at_t(B_z,r_probe,z_probe,date,shot,t,show_x
 %   boolean: save, option for saving the reconstruction result
 %   string: SXRfilename, name of the SXR image file
 %   boolean: filter, option for applying non-linear mean (NLM) filter
+%   boolean: NL, option for using non-linear reconstruction
 
 % 実行結果（行列）を保存するフォルダの確認
 % なければ作成＆計算、あれば読み込みsave
@@ -28,19 +29,22 @@ else
 end
 
 % 再構成計算に必要なパラメータを計算するなら読み込む、しない場合も範囲に関しては読み込む
-filepath = '/Users/shinjirotakeda/Documents/GitHub/SXR_diagnostics/parameters.mat';
+filepath = '/Users/shinjirotakeda/Documents/GitHub/test-open/Soft X-ray/parameters.mat';
 if clc_flag
     N_projection_new = 80;
     N_grid_new = 100;
     if isfile(filepath)
-        load(filepath, 'U1', 'U2', 's1', 's2', 'v1', 'v2', 'M', 'K', 'range','N_projection', 'N_grid');
+        load(filepath, 'gm2d1', 'gm2d2', 'U1', 'U2', 's1', 's2', 'v1', 'v2', 'M', 'K', 'range','N_projection', 'N_grid');
+%         load(filepath);
         if N_projection_new ~= N_projection || N_grid_new ~= N_grid
             disp('Different parameters - Start calculation!');
-            [U1,U2,s1,s2,v1,v2,M,K] = clc_parameters(N_projection_new,N_grid_new);
+            clc_parameters(N_projection_new,N_grid_new);
+            load(filepath, 'gm2d1', 'gm2d2', 'U1', 'U2', 's1', 's2', 'v1', 'v2', 'M', 'K', 'range');
         end
     else
         disp('No parameter - Start calculation!');
-        [U1,U2,s1,s2,v1,v2,M,K] = clc_parameters(N_projection_new,N_grid_new);
+        clc_parameters(N_projection_new,N_grid_new);
+        load(filepath, 'gm2d1', 'gm2d2', 'U1', 'U2', 's1', 's2', 'v1', 'v2', 'M', 'K', 'range');
     end
 else
     load(filepath,'range');
@@ -58,8 +62,8 @@ if clc_flag
     end
 
 %         再構成計算
-    EE_high = clc_distribution(M,K,U1,s1,v1,VectorImage1,plot_flag);
-    EE_low = clc_distribution(M,K,U2,s2,v2,VectorImage2,plot_flag);
+    EE_high = clc_distribution(M,K,gm2d1,U1,s1,v1,VectorImage1,plot_flag,NL);
+    EE_low = clc_distribution(M,K,gm2d2,U2,s2,v2,VectorImage2,plot_flag,NL);
 else
     loadpath_high = strcat(savefolder,'/',num2str(number),'_high.txt');
     loadpath_low = strcat(savefolder,'/',num2str(number),'_low.txt');
