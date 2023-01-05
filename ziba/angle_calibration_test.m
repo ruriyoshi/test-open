0a97a02ab0c33c64d09ca0900fe794419d83ade7
dokeyuka <dokeyuka@g.ecc.u-tokyo.ac.jp>
2023-01-05 20:37:43 +0900

    change name

--- ziba/angle_calibration_test.m
+++ ziba/angle_calibration_test.m
@@ -5,8 +5,10 @@ clear all
 %direction_num : x, y, z direction → 3
 
 t = 1:200;
+T = length(t);
 total_channel_num = 24;
 probe_num = 8;
+
 direction_num = total_channel_num/probe_num; 
 
 test_Bt_raw = zeros(length(t), probe_num); % 200*8
@@ -14,12 +16,12 @@ test_Bz_raw = zeros(length(t), probe_num);
 
 
 for i = 1:length(t)
-    T = length(t);
+   
     offset = 0.05;
 
     %test_Bt_raw(i,:) = sin(2*pi*i/T); test_Bz_raw(i,:) = cos(2*pi*i/T);
-    %test_Bz_raw(i,:) = sin(2*pi*i/T); test_Bt_raw(i,:) = cos(2*pi*i/T);
-    test_Bt_raw(i,:) = 1; test_Bz_raw(i,:) = 0;
+    test_Bz_raw(i,:) = sin(2*pi*i/T); test_Bt_raw(i,:) = cos(2*pi*i/T);
+    %test_Bt_raw(i,:) = 1; test_Bz_raw(i,:) = 0;
     %test_Bt_raw(i,:) = 0; test_Bz_raw(i,:) = 1;
     %test_Bt_raw(i,:) = 2*sin(2*pi*i/T); test_Bz_raw(i,:) = sin(2*pi*i/T);
 
@@ -27,7 +29,7 @@ end
 
 
 
-% signal format: n_time x n_channel (example: 1000 x 8 for real experiment)
+% signal format: n_time x n_probe (example: 1000 x 8 for real experiment)
 % can subsitude test_signal with real signal in real experiment
 
 %initialization
@@ -45,14 +47,15 @@ for i = 1:length(t)
     theta(i,:) = atan(test_Bz_raw(i,:)/test_Bt_raw(i,:)); % 1*channel_num
   
 
-    % for j = 1:probe_num
-    %   mat = [cos(theta(i,j)) sin(theta(i,j)); -sin(theta(i,j)) cos(theta(i,j))];
-    %   B(:,i,j) = mat*[test_Bt_raw(i,j); test_Bz_raw(i,j)];
-    %   Bt(i,j) = B(1,i,j);
-    %   Bz(i,j) = B(2,i,j);theta
-    %end
+    for j = 1:probe_num
+       mat = [cos(theta(i,j)) sin(theta(i,j)); -sin(theta(i,j)) cos(theta(i,j))];
+       B(:,i,j) = mat*[test_Bt_raw(i,j); test_Bz_raw(i,j)];
+       Bt(i,j) = B(1,i,j);
+       Bz(i,j) = B(2,i,j);
+    end
 
-    %initialization
+    %{
+    initialization
     B = zeros(1,probe_num,2);
     mat = zeros(2,2,probe_num);
     
@@ -60,14 +63,14 @@ for i = 1:length(t)
     B = mat*[test_Bt_raw(i,:); test_Bz_raw(i,:)];
     Bt(i,:) = B(1,i,:);
     Bz(i,:) = B(2,i,:);
-
+    %}
 end
 
     f = figure;
    % for k = 1:probe_num
         k = 8; hold on;
-        %plot(t,test_Bt_raw(:,k),'o','color','r','markersize',3);
-        %plot(t,test_Bz_raw(:,k),'o','color','b','markersize',3);
+        plot(t,test_Bt_raw(:,k),'o','color','r','markersize',3);
+        plot(t,test_Bz_raw(:,k),'o','color','b','markersize',3);
         %legend('Bt_ raw','Bz_ raw');
         plot(t, Bt(:,k),'*','color','r','markersize',3);  
         plot(t, Bz(:,k),'*','color','b','markersize',3);
