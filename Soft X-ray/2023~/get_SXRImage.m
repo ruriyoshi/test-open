2fa83b79be880b4e00851147188af4b05d29dd81
shinjiro1214 <shinjiro.takeda@gmail.com>
2023-05-07 12:23:43 +0900

    4視点用フォルダ作成

--- Soft X-ray/2023~/get_SXRImage.m
+++ Soft X-ray/2023~/get_SXRImage.m
@@ -7,8 +7,8 @@ function [VectorImage1,VectorImage2] = get_SXRImage(date,number,SXRfilename,filt
 % % 画像を切り取る
 N_projection = 80;
 % VectorImages = CutImage(date,shot,N_projection/230,false);
-% CheckFlag = true;
-CheckFlag = false;
+CheckFlag = true;
+% CheckFlag = false;
 VectorImages = CutImage(date,N_projection,CheckFlag,SXRfilename,filter);
 VectorImages1 = squeeze(VectorImages(1,:,:));
 VectorImages2 = squeeze(VectorImages(2,:,:));
@@ -33,27 +33,51 @@ end
 
 PositionPath = strcat('/Users/shinjirotakeda/OneDrive - The University of Tokyo/Documents/SXR_Images/',num2str(date),'/PositionCheck.tif');
 CalibrationImage = imread(PositionPath);
-[centers,radii]=FindFibers(CalibrationImage);
+if date == 230315
+    centers = 1.0e3*[...
+        0.2815,1.5525;0.4834,1.5468;0.8174,1.5699;1.0084,1.5579;...
+        1.0215,1.1871;0.8132,1.1972;0.4748,1.1887;0.2770,1.1942;...
+        0.2766,0.8066;0.4750,0.7979;0.8203,0.8339;1.0226,0.8280;...
+        1.0169,0.4307;0.8187,0.4377;0.4819,0.4108;0.2794,0.4245];
+    IW = 95;
+elseif date == 230119
+    centers = 1.0e3*[...
+        0.2815,1.5525;0.5234,1.5168;0.8174,1.5699;1.0584,1.5279;...
+        1.0665,1.1621;0.8532,1.1972;0.5248,1.1487;0.3370,1.1542;...
+        0.3366,0.7766;0.5250,0.7679;0.8753,0.7939;1.0676,0.7830;...
+        1.0669,0.4107;0.8787,0.4177;0.5319,0.3808;0.3394,0.3945];
+    IW = 90;
+elseif date == 230316
+    [centers,radii]=FindFibers(CalibrationImage,[90,130]);
+    IW = 90;
+    centers(:,1) = centers(:,1) + 40;
+    centers(:,2) = centers(:,2) - 20;
+else
+    [centers,radii]=FindFibers(CalibrationImage,[70,130]);
+    IW = 85;
+end
+% 上にずらすときは1つ目の数字を増やす
+% 右にずらすときは2つめの数字を増やす
 % IW = round(mean(radii));
 centers = round(centers);
-IW = 80;
+
 
 Center = zeros(2,8,2);
 TimeRapsImage = zeros(2,8,2*IW,2*IW);
 
 Center(1,:,:) = centers([1,3,6,8,9,11,14,16],:);
 Center(2,:,:) = centers([2,4,5,7,10,12,13,15],:);
-
-Center(2,1,:) = [530,1520];
-Center(2,2,:) = [1060,1530];
-Center(2,3,:) = [1070,1160];
-Center(2,4,:) = [530,1150];
-Center(2,5,:) = [530,770];
-Center(2,6,:) = [1070,780];
-Center(2,7,:) = [1070,410];
-Center(2,8,:) = [530,380];
-Center(1,:,1) = Center(2,:,1) - 190;
-Center(1,:,2) = Center(2,:,2) + 5;
+% 
+% Center(2,1,:) = [530,1520];
+% Center(2,2,:) = [1060,1530];
+% Center(2,3,:) = [1070,1160];
+% Center(2,4,:) = [530,1150];
+% Center(2,5,:) = [530,770];
+% Center(2,6,:) = [1070,780];
+% Center(2,7,:) = [1070,410];
+% Center(2,8,:) = [530,380];
+% Center(1,:,1) = Center(2,:,1) - 190;
+% Center(1,:,2) = Center(2,:,2) + 5;
 
 % disp(Center);
 % whos RawImage
@@ -80,9 +104,9 @@ if check_flag
     f1=figure;
 %     f.WindowState = 'maximized';
     f1.Position = [900,200,500,500];
-    f2=figure;
-%     f.WindowState = 'maximized';
-    f2.Position = [900,200,500,500];
+%     f2=figure;
+% %     f.WindowState = 'maximized';
+%     f2.Position = [900,200,500,500];
 end
 
 CalibrationPath = strcat('/Users/shinjirotakeda/OneDrive - The University of Tokyo/Documents/SXR_Images/',num2str(date),'/CalibrationFactor.mat');
@@ -124,13 +148,13 @@ for i=1:8
 %         if i == 8
 %             colorbar;
 %         end
-        figure(f2);
-        RoughCalibrated1 = RoughImage1;
-        RoughCalibrated1(k) = RoughCalibrated1(k).*squeeze(CalibrationFactor(1,i,:));
-        subplot(4,4,2*(i-1)+1);imagesc(RoughCalibrated1);title(title1);
-        RoughCalibrated2 = RoughImage2;
-        RoughCalibrated2(k) = RoughCalibrated2(k).*squeeze(CalibrationFactor(1,i,:));
-        subplot(4,4,2*(i-1)+2);imagesc(RoughCalibrated2);title(title2);
+        % figure(f2);
+        % RoughCalibrated1 = RoughImage1;
+        % RoughCalibrated1(k) = RoughCalibrated1(k).*squeeze(CalibrationFactor(1,i,:));
+        % subplot(4,4,2*(i-1)+1);imagesc(RoughCalibrated1);title(title1);
+        % RoughCalibrated2 = RoughImage2;
+        % RoughCalibrated2(k) = RoughCalibrated2(k).*squeeze(CalibrationFactor(1,i,:));
+        % subplot(4,4,2*(i-1)+2);imagesc(RoughCalibrated2);title(title2);
     end
     
     % figure;imagesc(RoughImage1);
@@ -139,8 +163,8 @@ for i=1:8
 
     VectorImages(1,i,:) = RoughImage1(k);
     VectorImages(2,i,:) = RoughImage2(k);
-%     VectorImages(1,i,:) = RoughImage1(k).*squeeze(CalibrationFactor(1,i,:));
-%     VectorImages(2,i,:) = RoughImage2(k).*squeeze(CalibrationFactor(2,i,:));
+    % VectorImages(1,i,:) = RoughImage1(k).*squeeze(CalibrationFactor(1,i,:));
+    % VectorImages(2,i,:) = RoughImage2(k).*squeeze(CalibrationFactor(2,i,:));
 end
 
 end
