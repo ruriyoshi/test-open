34434ce1a40242a4c697bc8e3b3d8537c3dca44a
shinjiro1214 <shinjiro.takeda@gmail.com>
2023-05-12 12:08:05 +0900

    4視点全体のテストに拡張
    4視点分のコードを追加
    実際の画像を使用できるようファントムモデルを追加

--- Soft X-ray/Four-View_Simulation/plot_SXR_test.m
+++ Soft X-ray/Four-View_Simulation/plot_SXR_test.m
@@ -2,8 +2,13 @@ function plot_SXR_test()
 
 NL = false;
 
-N_projection_new = 30;
-N_grid_new = 40;
+N_projection_new = 80;
+N_grid_new = 100;
+% N_projection_new = 40;
+% N_grid_new = 50;
+
+% 'econ'の有無で実行時間比較
+% 基本N_projection>N_gridなので'econ'でいい
 
 % 再構成計算に必要なパラメータを計算するなら読み込む、しない場合も範囲に関しては読み込む
 filepath = '/Users/shinjirotakeda/Documents/GitHub/test-open/Soft X-ray/Four-View_Simulation/parameters.mat';
@@ -26,8 +31,18 @@ end
     
 % number = (t-start)/interval+1;
 plot_flag = false;
-[~,Iwgn] = Assumption(N_projection,gm2d1,true);
-EE = clc_distribution(M,K,gm2d1,U1,s1,v1,Iwgn,plot_flag,NL);
+% [~,Iwgn1] = Assumption(N_projection,gm2d1,true);
+% [~,Iwgn2] = Assumption(N_projection,gm2d2,false);
+% [~,Iwgn3] = Assumption(N_projection,gm2d3,false);
+% [~,Iwgn4] = Assumption(N_projection,gm2d4,false);
+[~,Iwgn1] = Assumption_2(N_projection,gm2d1,true);
+[~,Iwgn2] = Assumption_2(N_projection,gm2d2,false);
+[~,Iwgn3] = Assumption_2(N_projection,gm2d3,false);
+[~,Iwgn4] = Assumption_2(N_projection,gm2d4,false);
+EE1 = clc_distribution(M,K,gm2d1,U1,s1,v1,Iwgn1,plot_flag,NL);
+EE2 = clc_distribution(M,K,gm2d2,U2,s2,v2,Iwgn2,plot_flag,NL);
+EE3 = clc_distribution(M,K,gm2d3,U3,s3,v3,Iwgn3,plot_flag,NL);
+EE4 = clc_distribution(M,K,gm2d4,U4,s4,v4,Iwgn4,plot_flag,NL);
 
 % f = figure;
 % f.Units = 'normalized';
@@ -40,8 +55,8 @@ zmin = range(1);
 zmax = range(2);
 rmin = range(5);
 rmax = range(6);
-r_space_SXR = linspace(rmin,rmax,size(EE,1));
-z_space_SXR = linspace(zmin,zmax,size(EE,2));
+r_space_SXR = linspace(rmin,rmax,size(EE1,1));
+z_space_SXR = linspace(zmin,zmax,size(EE1,2));
 
 r_range = find(0.060<=r_space_SXR & r_space_SXR<=0.330);
 r_space_SXR = r_space_SXR(r_range);
@@ -49,12 +64,35 @@ z_range = find(-0.17<=z_space_SXR & z_space_SXR<=0.17);
 
 z_space_SXR = z_space_SXR(z_range);
 
-EE = EE(r_range,z_range);
+EE1 = EE1(r_range,z_range);
+EE2 = EE2(r_range,z_range);
+EE3 = EE3(r_range,z_range);
+EE4 = EE4(r_range,z_range);
 
-figure;
+subplot(2,2,1);
 [SXR_mesh_z,SXR_mesh_r] = meshgrid(z_space_SXR,r_space_SXR);
-[~,h1] = contourf(SXR_mesh_z,SXR_mesh_r,EE,20);
+[~,h1] = contourf(SXR_mesh_z,SXR_mesh_r,EE1,20);
 h1.LineStyle = 'none';
 c=colorbar;c.Label.String='Intensity [a.u.]';c.FontSize=18;
+title('1');
+
+subplot(2,2,2);
+[~,h2] = contourf(SXR_mesh_z,SXR_mesh_r,EE2,20);
+h2.LineStyle = 'none';
+c=colorbar;c.Label.String='Intensity [a.u.]';c.FontSize=18;
+title('2');
+
+subplot(2,2,3);
+[~,h3] = contourf(SXR_mesh_z,SXR_mesh_r,EE3,20);
+h3.LineStyle = 'none';
+c=colorbar;c.Label.String='Intensity [a.u.]';c.FontSize=18;
+title('3');
+
+subplot(2,2,4);
+[~,h4] = contourf(SXR_mesh_z,SXR_mesh_r,EE4,20);
+h4.LineStyle = 'none';
+c=colorbar;c.Label.String='Intensity [a.u.]';c.FontSize=18;
+title('4');
+
 
 end
